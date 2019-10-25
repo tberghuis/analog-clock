bd6e5f861abc146376148a2cd5a086e41ce67e6e
Tom Berghuis <thomas.berghuis@gmail.com>
2019-10-26 10:12:01 +1100

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,14 @@ function App() {
   const { hours, minutes, seconds } = useTime();
   return (
     <Svg viewBox="0 0 300 300">
-      <circle cx="150" cy="150" r="100" fill="green" />
+      <circle
+        cx="150"
+        cy="150"
+        r="110"
+        stroke="black"
+        fill="transparent"
+        strokeWidth="10"
+      />
       {/* hours */}
       <g
         transform={`rotate(${hours * 30 + (minutes / 60) * 30},150,150)`}
@@ -32,12 +39,11 @@ function App() {
         <line strokeWidth="2" x1="150" y1="150" x2="150" y2="70" />
       </g>
       {/* seconds */}
-      <g
+      <SecondsHand
         transform={`rotate(${seconds * 6},150,150)`}
-        style={{ stroke: "black" }}
       >
         <line x1="150" y1="150" x2="150" y2="50" />
-      </g>
+      </SecondsHand>
 
       {markers.map((m, i) => (
         <React.Fragment key={i}>{drawMarker(m)}</React.Fragment>
@@ -59,6 +65,12 @@ const Svg = styled.svg`
   /* background-color: pink; */
 `;
 
+const SecondsHand = styled.g`
+  stroke: black;
+`;
+const MinutesHand = styled.g``;
+const HoursHand = styled.g``;
+
 const markers = Array.from(new Array(60), (x, i) => i);
 const markers5Min = markers.filter(m => m % 5 === 0);
 
@@ -72,7 +84,6 @@ const drawLabel = (label, markerPos) => {
         alignmentBaseline="central"
         x={80 * Math.cos((Math.PI / 30) * markerPos * 5)}
         y={80 * Math.sin((Math.PI / 30) * markerPos * 5)}
-        class="small"
       >
         {label}
       </text>
